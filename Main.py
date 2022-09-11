285dea8ebb8cbb504b85fb1eea994e9d5b277ccf
Ancel-Ishita <110010429+Ancel-Ishita@users.noreply.github.com>
2022-09-11 22:31:51 +0530

    Initial commit

--- Main.py
+++ Main.py
@@ -11,12 +11,31 @@ class Queue:
 
   def enqueue(self, data) -> None:
     # Write your code here
+    if self.last is None:
+      self.head = Node(data)
+      self.last = self.head
+    else:
+      self.last.next = Node(data)
+      self.last = self.last.next
 
   def dequeue(self) -> None:
     # Write your code here
-
+    if self.head is None:
+      return None
+    else:
+      removed_data = self.head.data
+      self.head = self.head.next
+    
   def status(self) -> None:
     # Write your code here
+    curr = self.head
+    display = []
+    while (curr):
+      display.append(curr.data)
+      curr = curr.next
+    for ele in display:
+      print(ele, end="=>")
+    print(None)
 
 
 # Do not change the following code
@@ -29,6 +48,6 @@ data = input_data.split(',')
 for i in range(len(operations)):
   if operations[i] == "enqueue":
     queue.enqueue(int(data[i]))
-  elif operations[i] == "dequeue":
+  elif operations[i] = "dequeue":
     queue.dequeue()
 queue.status()
